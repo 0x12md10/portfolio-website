06d769ec2e1b6abf502ab01f116a3d2906fa2a16
0x12md10 <abish102001@gmail.com>
2024-07-17 23:32:18 +0530

    hover animation

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,52 +1,56 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./Header.css";
 import { animated , useSpring } from "@react-spring/web";
 
 function Header() {
 
-  const [springs, api] = useSpring(()=> {
-    return {
-      from : {
-        x : -100
-      }
-    }
-  })
 
-  const [currentHoverSize, setCurrentHoverSize] = useState()
+  const [springProps, api] = useSpring(() => ({
+    top : '20',
+    left: '0',
+    width : '0',
+    height : '0',
+        visibility : 'hidden'
+  
+  }));
+
+
 
   function handleMouseOver(e) {
-    console.log(`${e.target.clientHeight}px`,e.target.clientHeight);
-    console.log(e)
+    const rect = e.target.getBoundingClientRect()
     api.start({
-      from : {
-        x : -100,
-        height : `${e.target.clientHeight}px`,
-        width : `${e.target.clientWidth}px`
-      },
-      to : {
-        x : 10,
-        height : `${e.target.clientHeight}px`,
-        width : `${e.target.clientWidth}px`
-      }
-    })
+      top : `${rect.top-10}px`,
+      left: `${rect.left-20}px`,
+      height : `${rect.height+20}px`,
+      width : `${rect.width + 40}px`,
+      visibility  : 'visible'
+  
+    });
   }
 
+  function handleMouseLeave() {
+    api.set({
+      width : '0',
+      height : '0',
+            visibility : 'hidden'
+    })
+  }
 
   return (
 
     <animated.div
-    
+    onMouseLeave={handleMouseLeave}
     className={'navbar'}
     
     >
         <animated.div
-          style={{
-            backgroundColor : "white",
-            borderRadius : '10px',
-            height : '20px',
-            width :'20px',
-            ...springs
-          }}
+        style={{
+          ...springProps,
+          position: 'absolute',
+          border: '3px solid rgba(255, 199, 237,1)',
+          borderRadius: '25px',
+          zIndex : '1000'
+        }}
         />
         <animated.div onMouseOver={(e)=> handleMouseOver(e)} className="navbar-item">/</animated.div>
         <animated.div onMouseOver={(e)=> handleMouseOver(e)}  className="navbar-item">skills</animated.div>
